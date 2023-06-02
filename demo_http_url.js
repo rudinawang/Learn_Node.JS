cbfe623f7dcdc010c27ac9c7d1e216ea15a83981
rudinawang <rudnawang07@gmail.com>
2023-06-02 23:15:57 +0545

    FridayDawn1

--- demo_http_url.js
+++ demo_http_url.js
@@ -2,6 +2,8 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!\n");
+    res.write("\n");
     res.write(req.url);
     res.end();
   })
