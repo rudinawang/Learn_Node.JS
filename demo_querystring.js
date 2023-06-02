e2913c4180f22c6c3416810eda64574c7f7323ba
rudinawang <rudnawang07@gmail.com>
2023-06-02 23:10:51 +0545

    fridayDawn

--- demo_querystring.js
+++ demo_querystring.js
@@ -4,8 +4,11 @@ var url = require("url");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.write("\n");
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
+    res.write("\n");
     res.end(txt);
   })
   .listen(8080);
